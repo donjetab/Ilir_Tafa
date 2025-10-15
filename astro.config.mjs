95ca98c2e78eca53b6944af50c3120b988a4fc9d
Donjeta Bajgora <donjetabajgora13@gmail.com>
2025-10-15 14:26:31 +0200

    Fix site URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://donjetab.github.io',
+  site: 'https://donjetab.github.io/Ilir_Tafa/',
   base: '/Ilir_Tafa/',
   integrations: [mdx(), sitemap()],
 
